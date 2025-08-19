9076683829d39aff8d2398591cc0cc9a45f1fadf
ifeisier <ifeisier@hotmail.com>
2025-08-19 21:20:15 +0800

    修复dispatch_mqtt_event问题

--- crates/core/src/mqtt_event/mod.rs
+++ crates/core/src/mqtt_event/mod.rs
@@ -6,11 +6,9 @@ use tokio::sync::mpsc;
 /// 分发处理 MQTT 事件.
 pub fn dispatch_mqtt_event(mut event_loop: mpsc::Receiver<Event>) {
     tokio::spawn(async move {
-        match event_loop.recv().await {
-            None => {
-                log::warn!("MQTT事件通道已关闭.");
-            }
-            Some(event) => log::info!("MQTT事件: {event:#?}"),
+        while let Some(event) = event_loop.recv().await {
+            log::info!("MQTT事件: {event:?}");
         }
+        log::warn!("MQTT事件通道已关闭.");
     });
 }
